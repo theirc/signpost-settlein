294b7af16ed37ffbc00ea62b4058d1bf65124938
Chris Anyango <chris@maisoninteractive.agency>
2023-08-04 18:10:57 +0300

    feat: deploy UAT changes, add map strings

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import HomePage, {
   HomePageStrings,
 } from '@ircsignpost/signpost-base/dist/src/home-page';
 import { MenuOverlayItem } from '@ircsignpost/signpost-base/dist/src/menu-overlay';
+import { ServiceMapProps } from '@ircsignpost/signpost-base/dist/src/service-map';
 import {
   CategoryWithSections,
   ZendeskArticle,
@@ -70,6 +71,7 @@ interface HomeProps {
   socialMediaLinks: SocialMediaLinks;
   // A list of |MenuOverlayItem|s to be displayed in the header and side menu.
   menuOverlayItems: MenuOverlayItem[];
+  serviceMapProps: ServiceMapProps;
   // The HTML text of the About Us category shown on the home page.
   aboutUsTextHtml: string;
   categories: ZendeskCategory[] | CategoryWithSections[];
@@ -84,6 +86,7 @@ const Home: NextPage<HomeProps> = ({
   headerBannerStrings,
   socialMediaLinks,
   menuOverlayItems,
+  serviceMapProps,
   aboutUsTextHtml,
   categories,
   footerLinks,
@@ -105,6 +108,7 @@ const Home: NextPage<HomeProps> = ({
       }}
       headerLogoProps={getHeaderLogoProps(currentLocale)}
       searchBarIndex={SEARCH_BAR_INDEX}
+      serviceMapProps={serviceMapProps}
       aboutUsTextHtml={aboutUsTextHtml}
       categories={categories}
       hasRecentArticles={USE_RECENT_ARTICLES}
