ebb6b78b72b1aacbc6971cfeff018a7c7e2469c1
Emilio Guisolfo <emilioguisolfo@gmail.com>
2023-10-11 11:30:24 -0300

    feat: bumped signpost-base 15.32.0 (#155)

--- pages/sections/[section].tsx
+++ pages/sections/[section].tsx
@@ -82,6 +82,7 @@ export default function Category({
       section={section}
       menuOverlayItems={menuOverlayItems}
       headerLogoProps={getHeaderLogoProps(currentLocale)}
+      footerLinks={footerLinks}
       searchBarIndex={SEARCH_BAR_INDEX}
       cookieBanner={
         <CookieBanner
@@ -240,6 +241,11 @@ export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
     populateMenuOverlayStrings(dynamicContent),
     categories,
     false
+  );
+
+  const footerLinks = getFooterItems(
+    populateMenuOverlayStrings(dynamicContent),
+    categories
   );
 
   const footerLinks = getFooterItems(
