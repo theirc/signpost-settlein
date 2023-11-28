d3bd6847d3b5a9ba8f0ab39850c7ee63d42c7be6
bgSignpost <132521292+bgSignpost@users.noreply.github.com>
2023-11-28 06:48:10 -0500

    Remove service map (#24)
    * Update index.tsx
    
    Removed service map
    
    * Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,6 @@ import HomePage, {
   HomePageStrings,
 } from '@ircsignpost/signpost-base/dist/src/home-page';
 import { MenuOverlayItem } from '@ircsignpost/signpost-base/dist/src/menu-overlay';
-import { ServiceMapProps } from '@ircsignpost/signpost-base/dist/src/service-map';
 import {
   CategoryWithSections,
   ZendeskArticle,
@@ -71,7 +70,6 @@ interface HomeProps {
   socialMediaLinks: SocialMediaLinks;
   // A list of |MenuOverlayItem|s to be displayed in the header and side menu.
   menuOverlayItems: MenuOverlayItem[];
-  serviceMapProps: ServiceMapProps;
   // The HTML text of the About Us category shown on the home page.
   aboutUsTextHtml: string;
   categories: ZendeskCategory[] | CategoryWithSections[];
@@ -86,7 +84,6 @@ const Home: NextPage<HomeProps> = ({
   headerBannerStrings,
   socialMediaLinks,
   menuOverlayItems,
-  serviceMapProps,
   aboutUsTextHtml,
   categories,
   footerLinks,
@@ -108,7 +105,6 @@ const Home: NextPage<HomeProps> = ({
       }}
       headerLogoProps={getHeaderLogoProps(currentLocale)}
       searchBarIndex={SEARCH_BAR_INDEX}
-      serviceMapProps={serviceMapProps}
       aboutUsTextHtml={aboutUsTextHtml}
       categories={categories}
       hasRecentArticles={USE_RECENT_ARTICLES}
