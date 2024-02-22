95f71112fedf7a3c6933750299d78e13fbe61f7d
Bianca Chami <b.chami39@gmail.com>
2024-02-22 09:51:40 +0000

    feat: added attachment

--- pages/articles/[article].tsx
+++ pages/articles/[article].tsx
@@ -11,8 +11,10 @@ import Footer from '@ircsignpost/signpost-base/dist/src/footer';
 import { MenuOverlayItem } from '@ircsignpost/signpost-base/dist/src/menu-overlay';
 import { createDefaultSearchBarProps } from '@ircsignpost/signpost-base/dist/src/search-bar';
 import {
+  Attachment,
   CategoryWithSections,
   ZendeskCategory,
+  fetchArticleAttachments,
   getArticle,
   getArticles,
   getCategories,
@@ -67,6 +69,7 @@ interface ArticleProps {
   pageUnderConstruction?: boolean;
   preview: boolean;
   strings: ArticlePageStrings;
+  attachments: Attachment[];
   // A list of |MenuOverlayItem|s to be displayed in the header and side menu.
   menuOverlayItems: MenuOverlayItem[];
   footerLinks?: MenuOverlayItem[];
@@ -84,6 +87,7 @@ export default function Article({
   pageUnderConstruction,
   preview,
   strings,
+  attachments,
   menuOverlayItems,
   footerLinks,
 }: ArticleProps) {
@@ -144,6 +148,7 @@ export default function Article({
           },
           strings: strings.articleContentStrings,
           previosURL: breadcrumbs,
+          attachments,
         }}
       />
     </ArticlePage>
@@ -196,6 +201,8 @@ export const getStaticProps: GetStaticProps = async ({
   locale,
   preview,
 }) => {
+  const articleId = Number(params?.article);
+
   const currentLocale = getLocaleFromCode(locale ?? '');
   let dynamicContent = await getTranslationsFromDynamicContent(
     getZendeskLocaleId(currentLocale),
@@ -255,6 +262,13 @@ export const getStaticProps: GetStaticProps = async ({
     preview ?? false
   );
 
+  const attachments = await fetchArticleAttachments(articleId, getZendeskUrl());
+
+  const articleWithAttachments = {
+    ...article,
+    attachments,
+  };
+
   // If article does not exist, return an error.
   if (!article) {
     const errorProps = await getErrorResponseProps(
@@ -301,6 +315,7 @@ export const getStaticProps: GetStaticProps = async ({
       articleTitle: article.title,
       articleId: article.id,
       articleContent: content,
+      attachments: articleWithAttachments.attachments,
       metaTagAttributes,
       siteUrl: getSiteUrl(),
       lastEditedValue: article.edited_at,
